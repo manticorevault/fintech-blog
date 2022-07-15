e105c1a8e190c22dfe490c02662aec89ec7f8f9d
Artur Serra <artur_seilah@hotmail.com>
2022-07-15 14:31:30 +0400

    Added the Categories widget and updated the menus

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Link from "next/link";
+import { getCategories } from '../services';
+
 
 const Categories = () => {
+
+  const [ categories, setCategories ] = useState<any>([]);
+
+  useEffect (() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, []);
+
   return (
-    <div>Categories</div>
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Categories
+      </h3>
+      { categories.map((category: { slug: React.Key | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => (
+        <Link key={ category.slug } href={`/category/${category.slug}`}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            { category.name }
+          </span>
+        </Link>
+      )) }
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home ({ posts } : { posts: any}) {
 
         <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
-              <PostWidget />
+              <PostWidget categories={undefined} slug={undefined}/>
               <Categories />
             </div>
         </div>
